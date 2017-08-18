792b7365b3ad4f91ff0364b1d537d1edeecc9623
SMuyesser <smuyesser@gmail.com>
2017-08-18 16:40:40 -0700

    startMoveOptions page working

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-	CLIENT_ORIGIN: 'https://themoviegame.netlify.com/' || 'http://localhost:3000/setup',
-	TMDB_API_KEY: '7e9a1ff04b7576b3330211792aa796b5'
+	CLIENT_ORIGIN: 'http://localhost:3000',
+	TMDB_API_KEY: '7e9a1ff04b7576b3330211792aa796b5',
+	PORT: process.env.PORT || 8080
 };
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
  const express = require('express');
  const cors = require('cors');
  const axios = require('axios');
+ const path = require('path');
+ const exphbs = require('express-handlebars');
  const bodyParser = require('body-parser');
  const morgan = require('morgan');
- const {CLIENT_ORIGIN, TMDB_API_KEY} = require('./config');
+ const {CLIENT_ORIGIN, TMDB_API_KEY, PORT} = require('./config');
 
  const app = express();
-
- const PORT = process.env.PORT || 8080;
-
  app.use(morgan('common'));
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
+
  app.use(
  	cors({
  		origin: CLIENT_ORIGIN
@@ -19,9 +23,10 @@
 
  app.get('/movieoptions/:title', (req, res) => {
  	axios.get('https://api.themoviedb.org/3/search/movie?api_key='+TMDB_API_KEY+'&language=en-US&query='+req.params.title+'&page=1&include_adult=false')
- 	.then(res => {
- 		console.log(res.data.results);
- 		return res.data.results;
+ 	.then(movies => {
+ 		console.log(movies.data.results);
+ 		const movieoptions = movies.data.results;
+ 		res.send(movieoptions);
  	})
  	.catch(error => {
  		console.error(error);
