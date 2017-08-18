20a3089d7346294e438a465907055dd641ed018c
SMuyesser <smuyesser@gmail.com>
2017-08-17 20:38:47 -0700

    api getting movie options, but not showing up in app

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+	CLIENT_ORIGIN: 'https://themoviegame.netlify.com/' || 'http://localhost:3000/setup',
+	TMDB_API_KEY: '7e9a1ff04b7576b3330211792aa796b5'
+};
--- server.js
+++ server.js
@@ -1,7 +1,32 @@
  const express = require('express');
+ const cors = require('cors');
+ const axios = require('axios');
+ const bodyParser = require('body-parser');
+ const morgan = require('morgan');
+ const {CLIENT_ORIGIN, TMDB_API_KEY} = require('./config');
+
  const app = express();
 
- const PORT = process.env.PORT || 3000;
+ const PORT = process.env.PORT || 8080;
+
+ app.use(morgan('common'));
+ app.use(
+ 	cors({
+ 		origin: CLIENT_ORIGIN
+ 	})
+ );
+ app.use(bodyParser.json());
+
+ app.get('/movieoptions/:title', (req, res) => {
+ 	axios.get('https://api.themoviedb.org/3/search/movie?api_key='+TMDB_API_KEY+'&language=en-US&query='+req.params.title+'&page=1&include_adult=false')
+ 	.then(res => {
+ 		console.log(res.data.results);
+ 		return res.data.results;
+ 	})
+ 	.catch(error => {
+ 		console.error(error);
+ 	})	
+ })
 
  app.get('/api/*', (req, res) => {
    res.json({ok: true});
