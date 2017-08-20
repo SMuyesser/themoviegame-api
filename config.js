4d9b829b3efa024bf8f62daafac2bbcd8a1e8da7
SMuyesser <smuyesser@gmail.com>
2017-08-20 15:29:08 -0700

    updated config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	CLIENT_ORIGIN: 'http://localhost:3000' || 'https://themoviegame.netlify.com',
+	CLIENT_ORIGIN: 'https://themoviegame.netlify.com' || 'http://localhost:3000',
 	TMDB_API_KEY: '7e9a1ff04b7576b3330211792aa796b5',
 	PORT: process.env.PORT || 8080
 };
