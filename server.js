dab2b6c6e05724f99071e1aa18045815be2c28ea
SMuyesser <smuyesser@gmail.com>
2017-08-22 05:49:05 -0700

    server and config updated

--- server.js
+++ server.js
@@ -54,11 +54,6 @@ app.get('/castmembermovies/:castMemberId', (req, res) => {
 	})
 })
 
-
-app.get('/api/*', (req, res) => {
-res.json({ok: true});
-});
-
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 module.exports = {app};
