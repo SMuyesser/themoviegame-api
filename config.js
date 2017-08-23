e7624b6ad184ebecc631a3961fd0c39f0037eae9
SMuyesser <smuyesser@gmail.com>
2017-08-23 13:14:46 -0700

    fixed config syntax

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
-	exports.CLIENT_ORIGIN = 'http://localhost:3000' || 'https://themoviegame.netlify.com',
-	exports.PORT = process.env.PORT || 8080,
+	exports.CLIENT_ORIGIN = 'http://localhost:3000' || 'https://themoviegame.netlify.com';
+	exports.PORT = process.env.PORT || 8080;
 /*	DATABASE_URL: process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/thevideoshelfdb',
 	TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/thevideoshelfdbtest',*/
-	exports.TMDB_API_KEY = process.env.TMDB_API_KEY
+	exports.TMDB_API_KEY = process.env.TMDB_API_KEY;
