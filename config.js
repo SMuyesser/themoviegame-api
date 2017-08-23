6efa61d19fffd28e4e59560f050b7a7a6066eb50
SMuyesser <smuyesser@gmail.com>
2017-08-23 15:43:50 -0700

    updated config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-	exports.CLIENT_ORIGIN = 'https://themoviegame.netlify.com' || 'https://localhost:3000' || 'http://localhost:3000';
+	exports.CLIENT_ORIGIN = 'https://themoviegame.netlify.com' || 'http://localhost:3000';
 	exports.PORT = process.env.PORT || 8000;
 	exports.API_KEY = process.env.TMDB_API_KEY;
 /*	DATABASE_URL: process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/thevideoshelfdb',
