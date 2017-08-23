198b7bbacfc522ad16442735bec0fb6683b9a21d
SMuyesser <smuyesser@gmail.com>
2017-08-23 13:42:08 -0700

    config update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const {CLIENT_ORIGIN, PORT, TMDB_API_KEY} = require('./config');
+const {CLIENT_ORIGIN, PORT, API_KEY} = require('./config');
 
 
 const app = express();
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 
 //get movie options
 app.get('/movieoptions/:title', (req, res) => {
-	axios.get('https://api.themoviedb.org/3/search/movie?api_key='+TMDB_API_KEY+'&language=en-US&query='+req.params.title+'&page=1&include_adult=false')
+	axios.get('https://api.themoviedb.org/3/search/movie?api_key='+API_KEY+'&language=en-US&query='+req.params.title+'&page=1&include_adult=false')
 	.then(movies => {
 		res.send(movies.data.results);
 	})
@@ -35,7 +35,7 @@ app.get('/movieoptions/:title', (req, res) => {
 
 //get movie cast list
 app.get('/moviedetails/:movieId', (req, res) => {
-	axios.get('https://api.themoviedb.org/3/movie/'+req.params.movieId+'/credits?api_key='+TMDB_API_KEY)
+	axios.get('https://api.themoviedb.org/3/movie/'+req.params.movieId+'/credits?api_key='+API_KEY)
 	.then(castinfo => {
 		res.send(castinfo.data);
 	})
@@ -46,7 +46,7 @@ app.get('/moviedetails/:movieId', (req, res) => {
 
 //get castmember's movies
 app.get('/castmembermovies/:castMemberId', (req, res) => {
-	axios.get('https://api.themoviedb.org/3/person/'+req.params.castMemberId+'/movie_credits?api_key='+TMDB_API_KEY)
+	axios.get('https://api.themoviedb.org/3/person/'+req.params.castMemberId+'/movie_credits?api_key='+API_KEY)
 	.then(movies => {
 		res.send(movies.data)
 	})
